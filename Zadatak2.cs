ac2cbd40da81517ca90374eb09dfa7951a7b8b6a
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2021-12-07 13:17:51 +0100

    Add files via upload

---
+++ Zadatak2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Definirajte klasu Stablo sa sljedećim članicama:
+a. varijabla tipa bool otpadajuListovi
+b. varijabla tipa string vrstaStabla
+c. svojstvo OtpadajuListovi (get() i set())
+d. konstruktor Stablo(bool) koji će dodijeliti vrijednost proslijeđenog argumenta
+varijabli otpadajuListovi
+e. override metode ToString()
+Definirajte objekt stablo1 tipa Stablo pomoću konstruktora te proslijedite vrijednost
+false. Definirajte objekt stablo2 tipa Stablo pomoću konstruktora te proslijedite
+vrijednost true.
+Ispišite podatke o objektima koristeći ToString() metodu, na način da ukoliko je
+vrijednost varijable otpadajuListovi true, vrijednost varijable vrstaStabla je
+bijelogorično, a ukoliko je vrijednost varijable otpadajuListovi false, vrijednost
+varijable vrstaStabla je crnogorično.*/
+
+namespace Ispit2
+{
+    internal class Program
+    {
+        class Stablo
+        {
+            public bool otpadajuListovi;
+            string vrstaStabla;
+
+            public void SetotpadajuListovi(bool value)
+            {
+                otpadajuListovi = value;
+            }
+
+            public bool GetotpadajuListovi()
+            {
+                return otpadajuListovi;
+            }
+
+            
+
+        }
+
+        static void Main(string[] args)
+        {
+            bool otpadajuListovi;
+            
+            Stablo stablo1 = new Stablo();
+           
+
+            Stablo stablo2 = new Stablo();
+
+
+            Console.WriteLine("Unesi 'true' ili 'false' ako stablu otpada lišće.");
+            otpadajuListovi = Convert.ToBoolean( Console.ReadLine());
+
+            if (otpadajuListovi == true)
+            {
+                Console.WriteLine("Stablo je bjelogorično.");
+            }
+
+            if (otpadajuListovi == false)
+            {
+                Console.WriteLine("Stablo je crnogorično.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zadatak1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*(6 bodova) Napišite program koji će zatražiti od korisnika da unese tri
+dvoznamenkasta broja te će izračunati i ispisati najveći od tri broja (npr. 12,17,15 ->
+17).*/
+
+namespace Ispit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int prviBroj;
+            int drugiBroj;
+            int treciBroj;
+            Console.WriteLine("Unesi 3 dvoznamenkasta broja: ");
+
+            prviBroj = Convert.ToInt32(Console.ReadLine());
+            drugiBroj = Convert.ToInt32(Console.ReadLine());
+            treciBroj = Convert.ToInt32(Console.ReadLine());
+
+            if (prviBroj > drugiBroj && prviBroj > treciBroj)
+            {
+                Console.WriteLine("Najveći broj je " + prviBroj);
+            }
+
+            if (drugiBroj > prviBroj && drugiBroj > treciBroj)
+            {
+                Console.WriteLine("Najveći broj je " + drugiBroj);
+            }
+
+            if (treciBroj > drugiBroj && treciBroj > prviBroj)
+            {
+                Console.WriteLine("Najveći broj je " +treciBroj);
+            }
+            Console.WriteLine("kraj");
+            Console.ReadKey();
+           
+            
+        }
+    }
+}
